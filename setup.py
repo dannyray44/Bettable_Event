5178ffe39397a4c3c47fb12f21f10cd00ce35f60
dannyray44 <dannyray44@hotmail.co.uk>
2023-02-24 19:14:35 +0100

    Package set up with apropriate default files

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-
-# Learn more: https://github.com/kennethreitz/setup.py
-
 from setuptools import setup, find_packages
-
 
 with open('README.rst') as f:
     readme = f.read()
@@ -12,13 +8,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='betable events',
     version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    description='Simple structure for handling events that can be bet on',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='Daniel Harris',
+    author_email='dannyray44@hotmail.co.uk',
+    url='https://github.com/dannyray44/betable_event',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
