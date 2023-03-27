8402529994653b2bd8bba699e5ac10da91e7ca0c
dannyray44 <dannyray44@hotmail.co.uk>
2023-03-27 09:10:12 +0200

    Complex type hinting to be removed

--- betting_event_new/bet.py
+++ betting_event_new/bet.py
@@ -1,18 +1,65 @@
-import enum
 import typing
+import enum
+import re
 from .bookmaker import Bookmaker
 # from .bet_types import MatchWinner, HomeAway
-from .type_hinting.bet_base import BetBase, BetType
+# from .type_hinting.bet_base import BetBase, BetType
+X = 2
 
-class Bet(BetBase):
+class BetType(enum.Enum):
+    """Enum of currently accepted bet types:
+    
+    Attributes:
+        MatchWinner: blah blah
+        AsianHandicap: ASIAN DOC"""
+    MatchWinner = 1
+    f"""{X}"""
+    # HomeAway = 2
+    AsianHandicap = 4
+    """ASIAN DOC"""
+    Goals_OverUnder = 5       
+    BothTeamsToScore = 8      
+    ExactScore = 10
+    DoubleChance = 12
+    Team_Total = 16
+    OddEven = 21
+    Team_OddEven = 23
+    Result_BothTeamsScore = 24
+    Result_TotalGoals = 25
+    TeamCleanSheet = 27
+    Team_WinToNil = 29
+    ExactGoalsNumber = 38
+    Team_ExactGoalsNumber = 40
+    Team_ScoreAGoal = 43
+
+ValueCheck: typing.Dict[BetType, typing.Pattern] = {
+    BetType.MatchWinner:            re.compile(r"^(home|draw|away)$"),
+    BetType.AsianHandicap:          re.compile(r"^(home|away) ([+-]?[0-9\.]{1,5})$"),
+    BetType.Goals_OverUnder:        re.compile(r"^(over|under) ([+-]?[0-9\.]{1,5})$"),
+    BetType.BothTeamsToScore:       re.compile(r"^(yes|no)$"),
+    BetType.ExactScore:             re.compile(r"^([0-9]{1,4}):([0-9]{1,4})$"),
+    BetType.DoubleChance:           re.compile(r"^(home|draw|away)\/(home|draw|away)$"),
+    BetType.Team_Total:             re.compile(r"^(home|away) (over|under) ([0-9]*)(.5)?$"),
+    BetType.OddEven:                re.compile(r"^(odd|even)$"),
+    BetType.Team_OddEven:           re.compile(r"^(home|away) (odd|even)$"),
+    BetType.Result_BothTeamsScore:  re.compile(r"^(home|draw|away)\/(yes|no)$"),
+    BetType.Result_TotalGoals:      re.compile(r"^(home|draw|away)\/(over|under) ([+-]?[0-9\.]{1,5})$"),
+    BetType.TeamCleanSheet:         re.compile(r"^(home|away) (yes|no)$"),
+    BetType.Team_WinToNil:          re.compile(r"^(home|away) (yes|no)$"),
+    BetType.ExactGoalsNumber:       re.compile(r"^(over|under) ([+-]?[0-9\.]{1,5})$"),
+    BetType.Team_ExactGoalsNumber:  re.compile(r"^(home|away) (over|under) ([+-]?[0-9\.]{1,5})$"),
+    BetType.Team_ScoreAGoal:        re.compile(r"^(home|away) (yes|no)$")
+}
+
+class Bet:
     DefaultBookmaker = Bookmaker()
 
     def __init__(self,
                  bet_type: typing.Union[BetType, int],
                  value: str,
                  odds: float,
-                 lay: bool,
                  bookmaker: typing.Optional[Bookmaker] = None,
+                 lay: bool = False,
                  volume: float = -1.0,
                  previous_wager: float = 0.0
                  ) -> None:
@@ -20,12 +67,16 @@ class Bet(BetBase):
 
         Args:
             bet_type (BetType | int): Bet type or bet type int as defined in bet.BetType.
-            value (str): Bet value. The accepted inputs of this is dependent on the bet_type.       #TODO: specify where to find
+            value (str): Bet value. The accepted inputs of this is dependent on the bet_type.
             odds (float): The odds for the bet.
+            bookmaker (Bookmaker, optional): The bookmaker for the bet. Defaults to Bookmaker(). If
+            not provided, the default bookmaker with no limits will be used.
+            volume (float, optional): The volume for the bet (only applies to exchanges). Defaults
+            to -1.0.
             lay (bool): True if the bet is a lay bet, False if it is a back bet.
-            bookmaker (Bookmaker, optional): The bookmaker for the bet. Defaults to Bookmaker(). If not provided, the default bookmaker with no limits will be used.
-            volume (float, optional): The volume for the bet (only applies to exchanges). Defaults to -1.0.
-            previous_wager (float, optional): The sum of any previous wagers placed on this bet. Defaults to 0.0. Useful for when a bet has been partially matched and you want to recalculate to minaize the risk of a losses.
+            previous_wager (float, optional): The sum of any previous wagers placed on this bet.
+            Defaults to 0.0. Useful for when a bet has been partially matched and you want to
+            recalculate to minaize the risk of a losses.
         """
         if bookmaker is None:
             bookmaker = self.DefaultBookmaker
@@ -38,6 +89,11 @@ class Bet(BetBase):
         self.volume: float = volume
         self.previous_wager: float = previous_wager
         self.wager: float = -1.0
+
+        if ValueCheck[self.bet_type].fullmatch(self.value) is None:
+            raise ValueError(f"Bet value '{self.value}' is not valid for bet type " +
+                             f"{self.bet_type.name} ({self.bet_type.value}).\nExpected format: " +
+                             f"\"{ValueCheck[self.bet_type].pattern}\"")
 
     def __eq__(self, __value: 'Bet') -> bool:
         return self.bet_type == __value.bet_type and self.bookmaker == __value.bookmaker and \
--- betting_event_new/type_hinting/bet_base.py
+++ betting_event_new/type_hinting/bet_base.py
@@ -1,6 +1,7 @@
 import enum
 import typing
-from . import bet_types as b_type
+from ..bookmaker import Bookmaker
+
 
 class BetType(enum.Enum):
     """Enum of currently accepted bet types:"""
@@ -22,77 +23,110 @@ class BetType(enum.Enum):
     Team_ExactGoalsNumber = 40
     Team_ScoreAGoal = 43
 
+# class BetBase:
+#     @typing.overload
+#     @classmethod
+#     def with_bet_type(cls, type: typing.Literal[BetType.MatchWinner, 1]) -> typing.Type[b_type.MatchWinner]: ...
+
+#     @typing.overload
+#     @classmethod
+#     def with_bet_type(cls, type: typing.Literal[BetType.AsianHandicap, 4]) -> typing.Type[b_type.AsianHandicap]: ...
+
+#     @typing.overload
+#     @classmethod
+#     def with_bet_type(cls, type: typing.Literal[BetType.Goals_OverUnder, 5]) -> typing.Type[b_type.Goals_OverUnder]: ...
+
+#     @typing.overload
+#     @classmethod
+#     def with_bet_type(cls, type: typing.Literal[BetType.BothTeamsToScore, 8]) -> typing.Type[b_type.BothTeamsToScore]: ...
+
+#     @typing.overload
+#     @classmethod
+#     def with_bet_type(cls, type: typing.Literal[BetType.ExactScore, 10]) -> typing.Type[b_type.ExactScore]: ...
+
+#     @typing.overload
+#     @classmethod
+#     def with_bet_type(cls, type: typing.Literal[BetType.DoubleChance, 12]) -> typing.Type[b_type.DoubleChance]: ...
+
+#     @typing.overload
+#     @classmethod
+#     def with_bet_type(cls, type: typing.Literal[BetType.Team_Total, 16]) -> typing.Type[b_type.Team_Total]: ...
+
+#     @typing.overload
+#     @classmethod
+#     def with_bet_type(cls, type: typing.Literal[BetType.OddEven, 21]) -> typing.Type[b_type.OddEven]: ...
+
+#     @typing.overload
+#     @classmethod
+#     def with_bet_type(cls, type: typing.Literal[BetType.Team_OddEven, 23]) -> typing.Type[b_type.Team_OddEven]: ...
+
+#     @typing.overload
+#     @classmethod
+#     def with_bet_type(cls, type: typing.Literal[BetType.Result_BothTeamsScore, 24]) -> typing.Type[b_type.Result_BothTeamsScore]: ...
+
+#     @typing.overload
+#     @classmethod
+#     def with_bet_type(cls, type: typing.Literal[BetType.Result_TotalGoals, 25]) -> typing.Type[b_type.Result_TotalGoals]: ...
+
+#     @typing.overload
+#     @classmethod
+#     def with_bet_type(cls, type: typing.Literal[BetType.TeamCleanSheet, 27]) -> typing.Type[b_type.TeamCleanSheet]: ...
+
+#     @typing.overload
+#     @classmethod
+#     def with_bet_type(cls, type: typing.Literal[BetType.Team_WinToNil, 29]) -> typing.Type[b_type.Team_WinToNil]: ...
+
+#     @typing.overload
+#     @classmethod
+#     def with_bet_type(cls, type: typing.Literal[BetType.ExactGoalsNumber, 38]) -> typing.Type[b_type.ExactGoalsNumber]: ...
+
+#     @typing.overload
+#     @classmethod
+#     def with_bet_type(cls, type: typing.Literal[BetType.Team_ExactGoalsNumber, 40]) -> typing.Type[b_type.Team_ExactGoalsNumber]: ...
+
+#     @typing.overload
+#     @classmethod
+#     def with_bet_type(cls, type: typing.Literal[BetType.Team_ScoreAGoal, 43]) -> typing.Type[b_type.Team_ScoreAGoal]: ...
+
+
+#     @classmethod
+#     def with_bet_type(cls, type: typing.Union[BetType, int]):
+#         return getattr(b_type, BetType(type).name)
+
 class BetBase:
-    @typing.overload
-    @classmethod
-    def with_bet_type(cls, type: typing.Literal[BetType.MatchWinner, 1]) -> typing.Type[b_type.MatchWinner]: ...
+    def __init__(self, bet_type: BetType, value: str, odds: float, bookmaker: typing.Optional[Bookmaker] = None, lay: bool = False, volume: float = -1, previous_wager: float = 0):
+        self.bet_type = bet_type
+        self.value = value
+        self.odds = odds
+        self.bookmaker = bookmaker
+        self.lay = lay
+        self.volume = volume
+        self.previous_wager = previous_wager
 
     @typing.overload
     @classmethod
-    def with_bet_type(cls, type: typing.Literal[BetType.HomeAway, 2]) -> typing.Type[b_type.HomeAway]: ...
+    def from_type(cls, bet_type: typing.Literal[BetType.MatchWinner, 1]) -> 'MatchWinner': ...
 
     @typing.overload
     @classmethod
-    def with_bet_type(cls, type: typing.Literal[BetType.AsianHandicap, 4]) -> typing.Type[b_type.AsianHandicap]: ...
-
-    @typing.overload
-    @classmethod
-    def with_bet_type(cls, type: typing.Literal[BetType.Goals_OverUnder, 5]) -> typing.Type[b_type.Goals_OverUnder]: ...
-
-    @typing.overload
-    @classmethod
-    def with_bet_type(cls, type: typing.Literal[BetType.BothTeamsToScore, 8]) -> typing.Type[b_type.BothTeamsToScore]: ...
-
-    @typing.overload
-    @classmethod
-    def with_bet_type(cls, type: typing.Literal[BetType.ExactScore, 10]) -> typing.Type[b_type.ExactScore]: ...
-
-    @typing.overload
-    @classmethod
-    def with_bet_type(cls, type: typing.Literal[BetType.DoubleChance, 12]) -> typing.Type[b_type.DoubleChance]: ...
-
-    @typing.overload
-    @classmethod
-    def with_bet_type(cls, type: typing.Literal[BetType.Team_Total, 16]) -> typing.Type[b_type.Team_Total]: ...
-
-    @typing.overload
-    @classmethod
-    def with_bet_type(cls, type: typing.Literal[BetType.OddEven, 21]) -> typing.Type[b_type.OddEven]: ...
-
-    @typing.overload
-    @classmethod
-    def with_bet_type(cls, type: typing.Literal[BetType.Team_OddEven, 23]) -> typing.Type[b_type.Team_OddEven]: ...
-
-    @typing.overload
-    @classmethod
-    def with_bet_type(cls, type: typing.Literal[BetType.Result_BothTeamsScore, 24]) -> typing.Type[b_type.Result_BothTeamsScore]: ...
-
-    @typing.overload
-    @classmethod
-    def with_bet_type(cls, type: typing.Literal[BetType.Result_TotalGoals, 25]) -> typing.Type[b_type.Result_TotalGoals]: ...
-
-    @typing.overload
-    @classmethod
-    def with_bet_type(cls, type: typing.Literal[BetType.TeamCleanSheet, 27]) -> typing.Type[b_type.TeamCleanSheet]: ...
-
-    @typing.overload
-    @classmethod
-    def with_bet_type(cls, type: typing.Literal[BetType.Team_WinToNil, 29]) -> typing.Type[b_type.Team_WinToNil]: ...
-
-    @typing.overload
-    @classmethod
-    def with_bet_type(cls, type: typing.Literal[BetType.ExactGoalsNumber, 38]) -> typing.Type[b_type.ExactGoalsNumber]: ...
-
-    @typing.overload
-    @classmethod
-    def with_bet_type(cls, type: typing.Literal[BetType.Team_ExactGoalsNumber, 40]) -> typing.Type[b_type.Team_ExactGoalsNumber]: ...
-
-    @typing.overload
-    @classmethod
-    def with_bet_type(cls, type: typing.Literal[BetType.Team_ScoreAGoal, 43]) -> typing.Type[b_type.Team_ScoreAGoal]: ...
-
-
+    def from_type(cls, bet_type: typing.Literal[BetType.AsianHandicap, 4]) -> 'AsianHandicap': ...
 
     @classmethod
-    def with_bet_type(cls, type: typing.Union[BetType, int]):
-        return getattr(b_type, BetType(type).name)
+    def from_type(cls, bet_type: typing.Union[BetType, int]):
+        return getattr(__name__, BetType(bet_type).name)
+
+
+class BetConstructor:
+    def __init__(self, bet_type: BetType):
+        self.bet_type = BetType(bet_type)
+
+    def __call__(self, *args, **kwargs):
+        raise NotImplementedError
+    
+class MatchWinner(BetConstructor):
+    def __call__(self, value: typing.Literal['home', 'draw', 'away'], odds: float, bookmaker: typing.Optional[Bookmaker] = None, lay: bool = False, volume: float = -1, previous_wager: float = 0):
+        return BetBase(BetType.MatchWinner, value, odds, bookmaker, lay, volume, previous_wager)
+    
+class AsianHandicap(BetConstructor):
+    def __call__(self, value: str, odds: float, bookmaker: typing.Optional[Bookmaker] = None, lay: bool = False, volume: float = -1, previous_wager: float = 0):
+        return BetBase(BetType.AsianHandicap, value, odds, bookmaker, lay, volume, previous_wager)
