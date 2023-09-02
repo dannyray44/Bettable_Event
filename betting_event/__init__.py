bf84ef82ee2cf879eb38a6d2cd986c75b14fc792
dannyray44 <dannyray44@hotmail.co.uk>
2023-09-02 12:40:55 +0200

    utils added to __init__

--- betting_event/__init__.py
+++ betting_event/__init__.py
@@ -1,3 +1,4 @@
 from .bet import Bet, BetType
 from .bookmaker import Bookmaker
 from .event import Event
+from .utils import american_to_decimal, decimal_to_american, decimal_to_fractional, fractional_to_decimal
